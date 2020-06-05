7920d6203840dbda1c3eea6f1b91a2acc012ca50
LiHongLiang <lihongliang@datagrand.com>
2020-06-06 00:42:12 +0800

    fix(add): pic

--- src/index.ts
+++ src/index.ts
@@ -7,8 +7,20 @@ const closeNav = () => {
   $('#home-nav').css('width', '0');
 }
 
+const bodyClose = () => {
+  if (parseInt($('#home-nav').css('width')) === 300) {
+    closeNav();
+  }
+}
+
 $('#spider-click').click(spiderMan);
 $('#menu-icon').click(openNav);
 $('#nav-close').click(closeNav);
+$('body').click(bodyClose);
+$(document).keydown((e: KeyboardEvent) => {
+  if (e.keyCode === 27) {
+    closeNav()
+  }
+})
 
 
